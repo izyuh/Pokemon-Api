3982028bd0b91be231e81b551feecbd9a837d3b2
Isaias Gutierrez <isaias.gutierrez48@gmail.comm>
2025-04-15 14:19:02 -0700

    added multiple fetches and images for each pokemon card

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let url = "https://pokeapi.co/api/v2/pokemon/";
 const container = document.getElementById("pokemon-list");
 const button = document.getElementById("load");
+
+const pokemonCountSpan = document.getElementById("pokemon-count");
 let count = 0;
+let pokemonCount = 0;
 
 async function fetchData() {
   if (button.innerHTML === "Fetch") {
@@ -11,27 +14,43 @@ async function fetchData() {
   const response = await fetch(url);
   const data = await response.json();
   const pokemons = data.results;
+  console.log(pokemons);
 
   pokemons.map((pokemon) => {
+    const a = document.createElement("a");
+    a.href = pokemon.url;
+    a.target = "_blank"; // Open in new tab
     const li = document.createElement("li");
+
+    //sets name to h3
     const capitalizedName =
       pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     li.innerHTML = `<h3>${capitalizedName}</h3>`;
+
+    //adds picture 
+    const img = document.createElement("img");
+    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonCount + 1}.png`;
+    li.appendChild(img);
+
+    //adds to list and makes hidden
     li.classList.add("hidden"); // Add hidden class initially
-    container.appendChild(li);
+    a.appendChild(li);
+    container.appendChild(a);
+    pokemonCount++;
+    pokemonCountSpan.innerHTML = `${pokemonCount}`;
   });
 
   observeElements(); // Observe new elements
-  console.log(data.results);
 
-  if (data.next && count < 2) {
+  //fetches next 8 pokemon pages
+  if (data.next && count < 9) {
     url = data.next;
     fetchData();
     count++;
-    console.log(count);
-  }else if(!data.next) {
-    button.style.display = "none"; // Hide button if no more data
-  } else {
+  } else if(!data.next) { // if there is no more pages hide the button to fetch more
+    button.style.display = "none"; 
+  } else {// if the count is over 8, set the url to the next page anyway and then reset the count
+    url = data.next;
     count = 0;
   }
 
@@ -53,4 +72,3 @@ function observeElements() {
   document.querySelectorAll(".hidden").forEach((li) => observer.observe(li));
 }
 
-button.addEventListener("click", fetchData);
