dbf637940dad2a1a186a44bef8800e3d651f5b19
isaias <isaias.gutierrez48@gmail.com>
2025-05-14 21:31:25 -0700

    added image placeholders and optimized rendering

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ const typeMap = new Map([
   ["normal", "gray"],
   ["fighting", "orange"],
   ["flying", "lightblue"],
-  ["poison", "purple"],
-  ["ground", "brown"],
+  ["poison", "#9f82ed"],
+  ["ground", "#94592c"],
   ["rock", "gray"],
   ["bug", "lightgreen"],
   ["ghost", "purple"],
   ["steel", "#5f646e"],
-  ["fire", "red"],
+  ["fire", "#eb3838"],
   ["water", "blue"],
   ["grass", "green"],
   ["electric", "#fff700"],
   ["psychic", "#f25ae6"],
   ["ice", "lightblue"],
   ["dragon", "darkblue"],
-  ["dark", "#0d2a45"],
+  ["dark", "#2c3030"],
   ["fairy", "#eda8f0"],
 ]);
 
@@ -49,8 +49,7 @@ function filter(input) {
 
 // fetch data from local storage if there is any //
 async function fetchData() {
-
-  if(savedData.length === 0) {
+  if (savedData.length === 0) {
     fetchA();
   } else {
     renderPokemon();
@@ -58,7 +57,7 @@ async function fetchData() {
 
   async function fetchA() {
     const data = await fetch(url).then((res) => res.json());
-    pokemons = data.results;  //gets initial data
+    pokemons = data.results; //gets initial data
 
     const pokemonDetails = await Promise.all(
       pokemons.map((pokemon) => fetch(pokemon.url).then((res) => res.json())) //gets detailed data
@@ -67,21 +66,74 @@ async function fetchData() {
     let iDetails = pokemonDetails.map((pokemon) => ({
       name: pokemon.name,
       id: pokemon.id,
-      sprite: pokemon.sprites.front_default || 'no image',
-      types: pokemon.types
-    }))
+      sprite: pokemon.sprites.front_default || "pokeball.png",
+      types: pokemon.types,
+    }));
 
     savedData.push(...iDetails); // pushes details to variable to save
+    localStorage.setItem("rawPokemon", JSON.stringify(savedData));
+    renderBatch(iDetails);
 
     if (data.next) {
       url = data.next;
       fetchA();
-      console.log('data fetched')
-    } else {
-      localStorage.setItem('rawPokemon', JSON.stringify(savedData));
-      renderPokemon();
     }
   }
+}
+
+function renderBatch(item) {
+  item.forEach((pokemon) => {
+    const a = document.createElement("a");
+    a.href = `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`;
+    a.target = "_blank"; // Open in new tab
+
+    const li = document.createElement("li"); //creates li for each pokemon
+
+    ////////////////////////////////////////////////////////add id////////////////////////////////////////////////////////
+
+    const id = document.createElement("span");
+    id.classList.add("id");
+    if (pokemon.id < 100) {
+      id.textContent = pokemon.id.toString().padStart(3, "0");
+    } else {
+      id.textContent = pokemon.id.toString();
+    }
+    li.appendChild(id);
+
+    ////////////////////////////////////////////////////////add name////////////////////////////////////////////////////////
+    const capitalizedName =
+      pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+
+    const nameHTML = document.createElement("h3");
+    nameHTML.innerHTML = capitalizedName;
+    li.appendChild(nameHTML);
+
+    ////////////////////////////////////////////////////////add image////////////////////////////////////////////////////////
+    const img = document.createElement("img");
+    img.src = pokemon.sprite; // Use the sprite URL from the detailed data
+    li.appendChild(img); // adds image to li
+
+    ////////////////////////////////////////////////////////add type////////////////////////////////////////////////////////
+    const typeDiv = document.createElement("div"); //makes div for types
+    const types = pokemon.types.map((type) => type.type.name); //loops through and sets types to pokemon types
+
+    types.forEach((type) => {
+      //for each type saved, create a span and capitalize the first letter
+      const span = document.createElement("span");
+
+      span.style.backgroundColor = typeMap.get(type); // Set background color based on type
+      span.innerHTML = type.charAt(0).toUpperCase() + type.slice(1);
+      typeDiv.appendChild(span); //add it to the div
+    });
+    li.appendChild(typeDiv);
+
+    ////////////////////////////////////////////////////////hides card for animation////////////////////////////////////////////////////////
+    // Add to list and make hidden
+    li.classList.add("hidden"); // Add hidden class initially
+    a.appendChild(li);
+
+    container.appendChild(a);
+  });
 }
 
 function renderPokemon() {
@@ -97,7 +149,10 @@ function renderPokemon() {
 
     const id = document.createElement("span");
     id.classList.add("id");
-    id.textContent = pokemon.id.toString().padStart(3, "0");
+    id.textContent = pokemon.id.toString();
+    if (pokemon.id < 100) {
+      id.textContent = pokemon.id.toString().padStart(3, "0");
+    }
     li.appendChild(id);
 
     ////////////////////////////////////////////////////////add name////////////////////////////////////////////////////////
@@ -110,7 +165,7 @@ function renderPokemon() {
 
     ////////////////////////////////////////////////////////add image////////////////////////////////////////////////////////
     const img = document.createElement("img");
-    img.src = pokemon.sprite || "no image"; // Use the sprite URL from the detailed data
+    img.src = pokemon.sprite; // Use the sprite URL from the detailed data
     li.appendChild(img); // adds image to li
 
     ////////////////////////////////////////////////////////add type////////////////////////////////////////////////////////
